c1f5107d3386e328e273486c0809a7fe49807227
xcdxcdxcd <757363054@qq.com>
2018-04-07 01:26:19 +0800

    fa

--- signup.php
+++ signup.php
@@ -3,15 +3,15 @@
   $dbuser = 'user';      
   $dbpass = 's951g015F';
   $conn = mysql_connect($dbhost, $dbuser, $dbpass);
-  $id=$_GET["id"];
-  $nm=$_GET["nm"];
-  $pwd=$_GET["pwd"];
-  $it=$_GET["it"];
-  $ot=$_GET["ot"];
-  $em=$_GET["em"];
-  $dp=$_GET["dp"];
-  if (mysql_num_rows(mysql_query("SELECT * FROM auth where id='".$id.";")) < 1){
-    mysql_query("SELECT * FROM auth where id='".$id.";")
+  $id=$_POST["id"];
+  $nm=$_POST["nm"];
+  $pwd=$_POST["pwd"];
+  $it=$_POST["it"];
+  $ot=$_POST["ot"];
+  $em=$_POST["em"];
+  $dp=$_POST["dp"];
+  if (mysql_num_rows(mysql_query("SELECT * FROM auth where id='".$id."';")) < 1){
+    mysql_query("SELECT * FROM auth where id='".$id."';")
     mysql_query("INSERT INTO auth (id, name, password, intime,outtime,email,dept) VALUES ('".$id."','".$nm."','".$pwd."','".$it."','".$ot."','".$em."','".$dp."')");
     setcookie("id", $id);
     echo 'succ';
